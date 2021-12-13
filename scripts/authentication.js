1a9b7f44213750902a39596942a0be73953d40e5
KevvKo <kklein915@gmail.com>
2021-12-14 00:09:23 +0100

    implement function to authenticate token

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -3,10 +3,40 @@ require('dotenv').config();
 
 const { JWT_SECRET } = process.env;
 
+/**
+ * 
+ * @param {String} username 
+ * @returns 
+ */
 function generateAccessToken( username ) {
     return jwt.sign(username, JWT_SECRET, { expireIn: '1800s' });
 }
 
+
+/**
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ * @returns 
+ */
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization'];
+    const token = authenticateToken && authHeader.split(' ')[1];
+
+    if(token === null) return res.sendStatus(401);
+
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        console.error(err);
+
+        if(err) return res.sendStatus(403);
+
+        req.user = user;
+        next();
+    });
+}
+
 module.exports = {
+    authenticateToken,
     generateAccessToken
 };
