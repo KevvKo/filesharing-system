8fc5c454e7dc4d07f76edf4e8100fb0e55ba0b01
KevvKo <kklein915@gmail.com>
2021-12-20 20:53:36 +0100

    add new svg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const bodyParser= require('body-parser');
-
+var path = require('path');
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var registrationRouter = require('./routes/registration');
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use('/static', express.static(path.join(__dirname, 'assets')));
 // Routes
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
