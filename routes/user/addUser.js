b408630cc5c312bab7c7ca69f4854c17e6894b6f
KevvKo <kklein915@gmail.com>
2022-01-06 23:33:43 +0100

    update errorhandling for reistration

--- routes/user/addUser.js
+++ routes/user/addUser.js
@@ -16,27 +16,43 @@ router.post( '/addUser', function (req,res) {
             password
         } = req.body;
     
-        bcrypt.hash(password, parseInt(SALT_ROUNDS), function(err, hash) {
 
-            if(!hash) { res.status(401).json({ error: "Uups, something went wrong. Please try again" });
-        }
-            const userDocument = {
-                name: username,
-                email: email,
-                password: hash
-            };
-            db.getDb()
-            .collection("user")
-            .insertOne(userDocument, function (error, result) {
-                if (error || !result) {
-                    res.status(400).json({ error: "Uups, something went wrong. Please try again" });
-                } else {
-                    console.log(`Added a new match with id ${result.insertedId}`);
-                    const token = generateAccessToken( username );
-                    res.json(token);
-                }
-            });
-        });   
+        const userDocument= {
+            name: username
+        };
+
+        db.getDb()
+        .collection("user")
+        .findOne(userDocument, function (err, result) {
+
+            if (result) {
+                res.status(409).json({ error: "This username already exists" });
+                return;     
+            } 
+
+            bcrypt.hash(password, parseInt(SALT_ROUNDS), function(err, hash) {
+                if(!hash) { res.status(401).json({ error: "Uups, something went wrong. Please try again" }); }
+                
+                const userDocument = {
+                    name: username,
+                    email: email,
+                    password: hash
+                };
+                
+                db.getDb()
+                .collection("user")
+                .insertOne(userDocument, function (error, result) {
+                    if (error || !result) {
+                        res.status(400).json({ error: "Uups, something went wrong. Please try again" });
+                    } else {
+                        console.log(`Added a new match with id ${result.insertedId}`);
+                        const token = generateAccessToken( username );
+                        res.json(token);
+                    }
+                });
+            });   
+        });
+        
     } catch {
         res.status(400).json({ error: "Uups, something went wrong. Please try again" });
     }
